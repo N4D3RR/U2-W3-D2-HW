95d5f95fa824b74b7625ed8e26fe98cd384b7351
NAD3R <naderdeghaili@gmail.com>
2025-11-04 16:59:38 +0100

    corrected timer

--- assets/timer.js
+++ assets/timer.js
@@ -1,13 +1,10 @@
-//var varie
 let seconds = 0
 const savedValue = sessionStorage.getItem("contatore")
 
-//verifica e recupero valore
 if (savedValue) {
-  seconds = parseInt(savedValue) // = savedValue in questo caso funziona uguale
+  seconds = savedValue // = savedValue in questo caso funziona uguale
 }
 
-// mostra il tempo
 function counter() {
   if (seconds === 1) {
     document.getElementById("counter").textContent = `${seconds} secondo`
@@ -16,10 +13,8 @@ function counter() {
   }
 }
 
-//avvia al caricamento della pagina
 counter()
 
-//cronometro, si aggiorna ogni secondo con counter()
 setInterval(() => {
   seconds++
   sessionStorage.setItem("contatore", seconds)
